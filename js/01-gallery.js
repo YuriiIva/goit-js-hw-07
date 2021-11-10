466cec89fcdf459b906bd4741e935e4612b5763c
Yurii <ivaschenko_u@ukr.net>
2021-11-11 00:45:41 +0200

    add 1 part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,10 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryRef = document.querySelector('.gallery');
+const galleryRef = document.querySelector(".gallery");
 
-const markup = galleryItems.map(({preview, original, description}) => {
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
     return `
      <a class="gallery__link" href="${original}">
     <img
@@ -13,9 +14,34 @@ const markup = galleryItems.map(({preview, original, description}) => {
       alt="${description}"
     />
   </a>`;
-}).join("");
-galleryRef.insertAdjacentHTML("beforeend",markup);
-console.log(galleryRef);
+  })
+  .join("");
+galleryRef.insertAdjacentHTML("beforeend", markup);
 
+let instance = null;
 
+function handlerImgclick(e) {
+  e.preventDefault();
+  const targetElem = e.target.tagName;
+  if (targetElem !== "IMG") {
+    return;
+  }
+  const previewT = e.target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${previewT}" width="800" height="600">
+`);
+  instance.show();
+}
+
+galleryRef.addEventListener("click", handlerImgclick);
+
+function handerEsc(e) {
+  if (e.code === "Escape") {
+      console.log(e);
+    instance.close();
+  }
+}
+
+window.addEventListener("keydown", handerEsc);
 console.log(galleryItems);
