4ba74bd742f661e13d6641524c6274dffa66789a
Yurii <ivaschenko_u@ukr.net>
2021-11-10 22:39:23 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) => {
+    return `
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+}).join("");
+galleryRef.insertAdjacentHTML("beforeend",markup);
+console.log(galleryRef);
+
+
 console.log(galleryItems);
