31986a2f22dfe445aaf25ac50466c6bc17d29ed6
Yurii <ivaschenko_u@ukr.net>
2021-11-11 14:36:35 +0200

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,25 +3,21 @@ import { galleryItems } from "./gallery-items.js";
 
 const galleryRef = document.querySelector(".gallery");
 
-const markup = galleryItems.map(({preview, original, description}) => {
-  return `<a class="gallery__item" href="${original}">
+const markUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>
     `;
-}).join("");
-galleryRef.insertAdjacentHTML("beforeend",markup);
+  })
+  .join("");
+galleryRef.insertAdjacentHTML("beforeend", markUp);
 
-const handerImgClick = (e) => {
-    e.preventDefault();
-    if(e.target.tagName !== "IMG") {
-        return;
-    }
-    let gallery = new SimpleLightbox('.gallery a');
-    gallery.on('show.simplelightbox', function () {
-        // do something…
-    });
-};
-galleryRef.addEventListener('click', handerImgClick);
+// galleryRef.addEventListener("click", handerImgClick);
 
+new SimpleLightbox(".gallery a", {
+  captions: "alt",
+  captionsDelay: 250,
+});
 
 console.log(galleryItems);
